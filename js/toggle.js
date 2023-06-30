f1759389591f231da7154b9c0bce40c8bfa94384
Customer <u1088-plrzrs8atjig@gcam1035.siteground.biz>
2023-06-30 00:10:39 +0000

    UPDATE: changes to get a better SEO in the website and create a minify version of Javascript files, with this we can improve in small percent the time in test of Lighthouse

--- js/toggle.js
+++ js/toggle.js
@@ -1,4 +1,3 @@
-/*Toggle dropdown list*/
 /*https://gist.github.com/slavapas/593e8e50cf4cc16ac972afcbad4f70c8*/
 
 var navMenuDiv = document.getElementById("nav-content");
@@ -7,18 +6,14 @@ var navMenu = document.getElementById("nav-toggle");
 document.onclick = check;
 function check(e) {
     var target = (e && e.target);
-    //Nav Menu
     if (!checkParent(target, navMenuDiv)) {
-        // click NOT on the menu
         if (checkParent(target, navMenu)) {
-            // click on the link
             if (navMenuDiv.classList.contains("hidden")) {
                 navMenuDiv.classList.remove("hidden");
             } else {
                 navMenuDiv.classList.add("hidden");
             }
         } else {
-            // click both outside link and outside menu, hide menu
             navMenuDiv.classList.add("hidden");
         }
     }
