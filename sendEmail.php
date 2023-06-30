5b8b9c3014861189409ff9a311b2fc1fc238652f
Customer <u1088-plrzrs8atjig@gcam1035.siteground.biz>
2023-06-30 20:16:38 +0000

    UPDATE: enable the emails Robert, caro, flor

--- sendEmail.php
+++ sendEmail.php
@@ -70,14 +70,14 @@ function sendEmail($language, $email, $name, $lastName, $number, $question, $lea
       $mail->setFrom('no-reply@abogadoericprice.com', 'No Reply');
     //   $mail->setFrom('support56@abogadoericprice.com');      
 
-    // $mail->addAddress('no-reply@abogadoericprice.com');
-    // $mail->addReplyTo('no-reply@abogadoericprice.com', 'No Reply');    
+    $mail->addAddress('no-reply@abogadoericprice.com');
+    $mail->addReplyTo('no-reply@abogadoericprice.com', 'No Reply');    
 
       // Correos a quienes le llegan
       $mail->addAddress('iku@abogadoericprice.com', 'Ivy Ku Flores');
-    //   $mail->addAddress('fmartinez@greencardla.com', 'Floriberta Martinez');
-    //   $mail->addAddress('support56@abogadoericprice.com', 'Paola Carolina');
-    //   $mail->addCC('rterrazas@greencardla.com', 'Robert Terrazas');
+      $mail->addAddress('fmartinez@greencardla.com', 'Floriberta Martinez');
+      $mail->addAddress('support56@abogadoericprice.com', 'Paola Carolina');
+      $mail->addCC('rterrazas@greencardla.com', 'Robert Terrazas');
       $mail->addCC('avelazquez2873@LosAngelesImmigration.onmicrosoft.com', 'Alberto Martinez');
 
       //Content
