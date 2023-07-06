66355795a6066be3837b4b77b16abf02365e61a0
Customer <u1088-plrzrs8atjig@gcam1035.siteground.biz>
2023-07-06 16:29:03 +0000

    FIX: clean the code, only remove different lines to have a better clean code

--- sendEmail.php
+++ sendEmail.php
@@ -4,7 +4,6 @@ use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\SMTP;
 use PHPMailer\PHPMailer\Exception;
 
-//Load Composer's autoloader
 require 'vendor/autoload.php';
 
 header("Cache-Control: no-cache, must-revalidate");
@@ -37,26 +36,24 @@ try {
 // Send a email
 function sendEmail($language, $email, $name, $lastName, $number, $question, $leadID)
 {            
-      $mail = new PHPMailer(true);
-      // Email Template
-      $message = file_get_contents('mailTemplate.html');
-      $message = str_replace('%language%', $language, $message);
-      $message = str_replace('%email%', $email, $message);
-      $message = str_replace('%name%', $name, $message);
-      $message = str_replace('%lastName%', $lastName, $message);
-      $message = str_replace('%mobile%', $number, $message);
-      $message = str_replace('%message%', $question, $message);
-      $message = str_replace('%leadID%', $leadID, $message);
+    $mail = new PHPMailer(true);
+    // Email Template
+    $message = file_get_contents('mailTemplate.html');
+    $message = str_replace('%language%', $language, $message);
+    $message = str_replace('%email%', $email, $message);
+    $message = str_replace('%name%', $name, $message);
+    $message = str_replace('%lastName%', $lastName, $message);
+    $message = str_replace('%mobile%', $number, $message);
+    $message = str_replace('%message%', $question, $message);
+    $message = str_replace('%leadID%', $leadID, $message);
 
-      if($leadID == "" || $leadID == null || $leadID == undefined) {
-          $leadID = 'This lead may already have an account with us';
-          $message = str_replace('%duplicate%','This lead may already have an account with us', $message);
-      } else {
-          $message = str_replace('%duplicate%','-', $message);
-      }
-      
-
-      //Server settings
+    if($leadID == "" || $leadID == null || $leadID == undefined) {
+        $leadID = 'This lead may already have an account with us';
+        $message = str_replace('%duplicate%','This lead may already have an account with us', $message);
+    } else {
+        $message = str_replace('%duplicate%','-', $message);
+    }
+            
     //   $mail->SMTPDebug = SMTP::DEBUG_SERVER; //<-- imprime todos los pasos que realiza el proceso de enviar correo
     $mail->isSMTP();
     $mail->Host = 'smtp.office365.com';
@@ -66,30 +63,27 @@ function sendEmail($language, $email, $name, $lastName, $number, $question, $lea
     $mail->SMTPSecure = 'tls';
     $mail->Port = 587;
 
-      //Correo saliente
-      $mail->setFrom('no-reply@abogadoericprice.com', 'No Reply');
-    //   $mail->setFrom('support56@abogadoericprice.com');      
+    $mail->setFrom('no-reply@abogadoericprice.com', 'No Reply');
 
     $mail->addAddress('no-reply@abogadoericprice.com');
-    $mail->addReplyTo('no-reply@abogadoericprice.com', 'No Reply');    
+    $mail->addReplyTo('no-reply@abogadoericprice.com', 'No Reply');
 
-      // Correos a quienes le llegan
-      $mail->addAddress('iku@abogadoericprice.com', 'Ivy Ku Flores');
-      $mail->addAddress('fmartinez@greencardla.com', 'Floriberta Martinez');
-      $mail->addAddress('support56@abogadoericprice.com', 'Paola Carolina');
-      $mail->addCC('rterrazas@greencardla.com', 'Robert Terrazas');
-      $mail->addCC('avelazquez2873@LosAngelesImmigration.onmicrosoft.com', 'Alberto Martinez');
+    // Correos a quienes le llegan
+    $mail->addAddress('iku@abogadoericprice.com', 'Ivy Ku Flores');
+    $mail->addAddress('fmartinez@greencardla.com', 'Floriberta Martinez');
+    $mail->addAddress('support56@abogadoericprice.com', 'Paola Carolina');
+    $mail->addCC('rterrazas@greencardla.com', 'Robert Terrazas');
+    $mail->addCC('avelazquez2873@LosAngelesImmigration.onmicrosoft.com', 'Alberto Martinez');
 
-      //Content
-      $mail->Encoding = 'base64';
-      $mail->CharSet = "UTF-8";
-        
-      $mail->isHTML(true);
-      $mail->Subject = 'Someone has opted in to contac form web site';
-      $mail->msgHTML($message); //Toma el template(mailTemplate.html) para construtir el contenido del correo
-      $mail->AltBody = 'Sending email'; // <-- Esta linea solo funciona para algun mensaje / NO SE UTILIZA puede quedar asi o comentada    
-      // Toma todos los parametros anteriorres y realiza el envio del correo
-      $mail->send();
+    //Content
+    $mail->Encoding = 'base64';
+    $mail->CharSet = "UTF-8";
+
+    $mail->isHTML(true);
+    $mail->Subject = 'Someone has opted in to contac form web site';
+    $mail->msgHTML($message); //Usa las variables a mandar y envia el correo usando el template
+    $mail->AltBody = 'Sending email'; // <-- Esta linea solo funciona para algun mensaje / NO SE UTILIZA puede quedar asi o comentada    com    
+    $mail->send();
 }
 
 // TRABAJANDO EN ESTA SECCION
