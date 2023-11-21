bfcdb292c4adb3f2464c4a46641a11c47b023a04
Customer <u1088-plrzrs8atjig@gcam1035.siteground.biz>
2023-11-21 20:17:24 +0000

    UPDATE: the test file, we try to can make the update in the data

--- test.php
+++ test.php
@@ -1,3 +1,48 @@
 <?php
 
-print('API TEST');
+$mobile = '8052984430';
+$email = 'jr.amaya062023@gmail.com';
+$lead_id = 19;
+
+$total_leads = getLeads($mobile, $email);
+
+if ($total_leads){
+    updateLead($lead_id);
+    echo "We have found another lead with this data";
+} else {
+    saveLead($name, $lastName, $mobile, $email);
+}
+
+// updateLead(18);
+
+function updateLead($lead_id) {
+    $host = "abogadoericprice.com";
+    $port = "5432";
+    $dbname = "dbezl1uquldojv";
+    $user = "uhgpgzxv2hhak";
+    $password = "700Flower!";
+
+    $connection_string = "host={$host} port={$port} dbname={$dbname} user={$user} password={$password} ";   
+    $dbconn = pg_connect($connection_string) or die('Could not reach database.');
+
+    // $sql = "UPDATE save_leads SET repeat='1' WHERE id_lead=".$lead_id;
+    $sql = "update save_leads set repeat = '1' where id_lead =".$lead_id;
+
+    return pg_affected_rows(pg_query($sql));
+}
+
+function getLeads($number, $email)
+{
+    $host = "abogadoericprice.com";
+    $port = "5432";
+    $dbname = "dbezl1uquldojv";
+    $user = "uhgpgzxv2hhak";
+    $password = "700Flower!";
+
+    $connection_string = "host={$host} port={$port} dbname={$dbname} user={$user} password={$password} ";
+    $dbconn = pg_connect($connection_string) or die('Could not reach database.');
+
+    $sql = "select lead_name, last_name, phone_number, email from save_leads where phone_number = '" . $number . "' or email = '" . $email . "' order by id_lead desc";
+    $result = pg_query($sql);
+    return pg_fetch_object($result);
+}
