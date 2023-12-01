688143edbffeb74fc80194381f9fcb10926416b7
Customer <u1088-plrzrs8atjig@gcam1035.siteground.biz>
2023-12-01 00:54:40 +0000

    FIX: remove the comparation tags when make the pull request and we fix the bug/conflict

--- blog.php
+++ blog.php
@@ -5,6 +5,7 @@ header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
 header("Cache-Control: post-check=0, pre-check=0", false);
 header("Pragma: no-cache");
 
+// Database data/connection
 $host = "abogadoericprice.com";
 $port = "5432";
 $dbname = "dbezl1uquldojv";
