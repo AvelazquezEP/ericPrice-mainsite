433e3eaf0a87a4f95719dc74d4ecf92fecb8cfaf
Customer <u1088-plrzrs8atjig@gcam1035.siteground.biz>
2023-12-06 21:56:58 +0000

    UPDATE: the new functions in the database

--- database.php
+++ database.php
@@ -1,15 +1,21 @@
   <?php
 
-
-
   $date = date('Y-m-d H:i:s.uO');
 
 // create_month("test", $date);
+// create_week($date, $date, 1, $date);
+$daily = 1;
+$paid_search = 2;
+$direct = 3;
+$organic_search = 4;
+$organic_social = 5;
+$referral = 6;
+$created_at = $date;
+$week_id = 16;
 
-create_week($date, $date, 1, $date);
+create_stat($daily, $paid_search, $direct, $organic_search, $organic_social, $referral, $created_at, $week_id);
 
-function create_month($name, $date)
-{
+function create_month($name, $date) {
     $host = "abogadoericprice.com";
     $port = "5432";
     $dbname = "dbezl1uquldojv";
@@ -37,6 +43,23 @@ function create_week($start_date, $finish_date, $month_id, $created_at){
       $sql = "INSERT INTO week_report(start_date, finish_date, month_id, created_at) " . "VALUES('" .cleanData($start_date) . "','" . cleanData($finish_date) . "','" . cleanData($i)  . "','" . cleanData($created_at) . "')";
       pg_affected_rows(pg_query($sql));
     }
+  }
+}
+
+function create_stat($daily, $paid_search, $direct, $organic_search, $organic_social, $referral, $created_at, $week_id)
+{
+  $host = "abogadoericprice.com";
+  $port = "5432";
+  $dbname = "dbezl1uquldojv";
+  $user = "uhgpgzxv2hhak";
+  $password = "700Flower!";
+
+  $connection_string = "host={$host} port={$port} dbname={$dbname} user={$user} password={$password}";
+  $dbconn = pg_connect($connection_string) or die('Could not reach database');
+
+  for($i=1; $i<=12; $i++) {
+    $sql = "INSERT INTO stats_report(daily, paid_search, direct, organic_search, organic_social, referral, created_at, week_id) " . "VALUES('" . $daily . "','" . $paid_search . "','" . $direct . "','" . $organic_search  . "','" . $organic_social . "','" . $referral . "','" . $created_at . "','" . $week_id .  "')";
+    return pg_affected_rows(pg_query($sql));
   }
 }
 
