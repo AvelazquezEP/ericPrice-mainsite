dfbc19da227216de5ad9926db424f0181d8e7ba7
Customer <u1088-plrzrs8atjig@gcam1035.siteground.biz>
2024-01-31 18:16:24 +0000

    UPDATE: configuration for the new office(Oxnard) and now we can create appointments in this location

--- apiForm.js
+++ apiForm.js
@@ -90,7 +90,7 @@ const createLeadApi = (first_name, last_name, email, mobile_phone, location_name
 
                 // Change the condition for the oxnard location only virtual
                 // if (location_name == "National" || location_name == "Oxnard-virtual") {
-                if (location_name == "National" || location_name == "Oxnard") {
+                if (location_name == "National") {
                     fullUrl = `https://greencardla.my.site.com/s/onlinescheduler?processId=a1h5f000000nAJZAA2&locationType=${byPhone}&WhatId=a1n5f0000006fzTAAQ&WhereID=${locationCode}&sumoapp_WhoId=0055f000007NE9T&clientId=${leadID}`;
                 } else {
                     fullUrl = `https://greencardla.my.site.com/s/onlinescheduler?processId=a1h5f000000nAJCAA2&locationType=${inPerson}&WhatId=a1n5f0000006fzTAAQ&WhereID=${locationCode}&sumoapp_WhoId=0055f000007NE9T&clientId=${leadID}`;
@@ -143,8 +143,8 @@ const getLocation = (location) => {
             code = OXCode;
             break;
         case "Oxnard":
-            // code = OXCode;
-            code = LACode;
+            code = OXCode;
+            // code = LACode;
             break;
         case "Riverside":
             code = RSCode;
