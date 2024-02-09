34fffe98ca2fee76ce3af80b0ca292c87b93da4d
Customer <u1088-plrzrs8atjig@gcam1035.siteground.biz>
2024-02-09 16:52:36 +0000

    REMOVE: the comments and code line we dont use anymore

--- apiForm.js
+++ apiForm.js
@@ -88,8 +88,6 @@ const createLeadApi = (first_name, last_name, email, mobile_phone, location_name
             }
             else {
 
-                // only in case need two different appointments
-                // Change the condition for the oxnard location only virtual
                 // if (location_name == "National" || location_name == "Oxnard-virtual") {
                 // if (location_name == "National" || location_name == "Bakersfield") { 
                 if (location_name == "National") {
